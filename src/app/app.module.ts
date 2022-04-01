7656b8d0a9f0a1058946c8ca1039ebd228e32fbd
Edson Kennis <edson.k.santos@outlook.com>
2022-04-01 16:58:54 -0300

    formulario criado e estilizado

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NovaTransferenciaComponent } from './nova-trasferencia/nova-transferencia.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
