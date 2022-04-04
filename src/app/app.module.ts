f0cac82da8f0badfee95db8aaa7d87f9fb8adef8
Edson Kennis <edson.k.santos@outlook.com>
2022-04-04 14:18:29 -0300

    Alterações do formulário do curso finaliado, iniciando parte de integração com API's

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NovaTransferenciaComponent } from './nova-trasferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
+import { LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovaTransferenciaComponent
+    NovaTransferenciaComponent,
+    ExtratoComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CommonModule,
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/nova-trasferencia/nova-transferencia.component.ts
+++ src/app/nova-trasferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, Output } from "@angular/core";
+import { EventEmitter } from "@angular/core";
 
 @Component({
     selector:'nova-transferencia',
@@ -8,4 +9,21 @@ import { Component } from "@angular/core";
 export class NovaTransferenciaComponent
 {
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada Nova trasnsferência');
+    const valorEmitir = {valor: this.valor, destino: this.destino}
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos()
+  {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
